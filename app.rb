69346f054b45ae6d3ae40b59667ce69cbeae6e58
Alicia Weenum <weenum.alicia@gmail.com>
2020-11-30 17:52:27 -0500

    commit tree 80ec84b2beb2fbfe0c92e19d6caa0700c0349f15

--- app.rb
+++ app.rb
@@ -1,6 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/' do
+        erb :index
+      end
+  
+      get "/info" do
+        erb :info
+      end 
 
+      get '/' do
+        "<h1>Buster's Best Bowling Alley</h1>"
+    end 
+
+    get '/' do
+        "<p>97 Bowling Alley Lane, Boulder CO </p>"
+    end
 
 end
